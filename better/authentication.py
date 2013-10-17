9001922461a3e969d6b96c33dd1dc36c4f98e22a
Skuli Freyr Hinriksson <skuli@sareye.com>
2013-10-17 11:11:08 +0200

    authentication now in a different file 2

--- better/authentication.py
+++ better/authentication.py
@@ -1,5 +1,6 @@
 __author__ = 'skuli'
 from twython import Twython
 def auth():
-    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    twitter = Twython("7pdvVotiZRfbJ1e7mpIVqA", "EpBnZpC1qKY6yJ0Uett07zrORpfb5XV7yQfSwoik", "1954450040-1pNgKK2bSIN7qRYhy7fUFdSgPhpTuzEKT3xXqCy"
+        , "pWpWbwp12xBoZcY3E3cQH4NiGpOLniNeYuoT6EFnXc")
     return twitter
